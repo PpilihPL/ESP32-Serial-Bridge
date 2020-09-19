cfc0d265d4c0687c8d1181eb7a8e06031e338f73
Philipp Luyken <philipp.luyken@st.oth-regensburg.de>
2020-09-19 10:41:33 +0200

    Ein Versuch eines Logins

--- vimp/vimp.py
+++ vimp/vimp.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url_login = 'https://elearning.uni-regensburg.de/login/index.php'
 url_startseite = 'https://elearning.uni-regensburg.de'
-url_meine_Kurse ='https://elearning.uni-regensburg.de/my/'
+url_meine_Kurse = 'https://elearning.uni-regensburg.de/my/'
 
 anmeldedaten_korrekt = {
             'username' : 'lup42010',
@@ -11,43 +11,77 @@ anmeldedaten_korrekt = {
             'realm' : 'hs',
           }
 
-anmeldedaten_fehlerhaft ={
-    'username': 'dsfa',
-    'password':'safd',
-    'realm':'gfds'
-    }
+def check(str_to_compare, str_to_compare_to):
+   if str_to_compare == str_to_compare_to:
+      print("anmeldung erfolgreich")
+      return True
+   else:
+      print("anmeldung fehlgeschlagen")
+      return False
+
+def login(website):
+
+    wahl = input('An Hochschule/Universät anmelden? (Hs/Uni)')
+    benutzername = input('Benutername: ')
+    passwort = input('Passwort: ')
+
+    anmeldedaten = {
+        wahl,
+        benutzername,
+        passwort
+        }
+
+    with requests.Session() as session:
+        if wahl == 'Hs':
+            print('Anmeldung an Hochschule wird durchgeführt...')
+            login = session.post(url_login, anmeldedaten)
+            login.text
+            after_login = BeautifulSoup(login.text, 'html.parser')
+            titel = after_login.title.text1
+        if wahl == 'Uni':
+            print('Anmeldung an Universität wird durchgeführt...')
+            login = session.post(url_login, anmeldedaten)
+            login.text
+            after_login = BeautifulSoup(login.text, 'html.parser')
+            titel = after_login.title.text1
+
+    #anmeldng im Elearning und erstellen des Objektes login
+    
+
+    anmeldung_erfolgreich = "GRIPS - Uni Regensburg"
+    check(titel, anmeldung_erfolgreich)
+    return True
+
 
 with requests.Session() as session:
-    #anmeldng im Elearning und erstellen des Objektes login
-    print('Anmeldung wird durchgeführt...')
-    login = session.post(url_login, data=anmeldedaten_korrekt)
-    login.text
-    after_login = BeautifulSoup(login.text, 'html.parser')
-    titel = after_login.title
+    ##anmeldng im Elearning und erstellen des Objektes login
+    #print('Anmeldung wird durchgeführt...')
+    #login = session.post(url_login, anmeldedaten_korrekt)
+    #login.text
+    #after_login = BeautifulSoup(login.text, 'html.parser')
+    #titel = after_login.title.text
 
-    anmeldung_erfolgreich= '<title>GRIPS - Uni Regensburg</title>'
+    #anmeldung_erfolgreich = "GRIPS - Uni Regensburg"
 
-    if titel is anmeldung_erfolgreich
-        print('anmeldung erfolgreich')
-    else:
-        print('anmeldung fehlgeschlagen')
+    #check(titel, anmeldung_erfolgreich)
 
+    #laden der Startseite in Ojekt
 
-    #print('Anmeldung2 wird durchgeführt...')
-    #login_2 = session.post(url_login, data=anmeldedaten_fehlerhaft)
-    #login_2.text
-    #after_login_2 = BeautifulSoup(login_2.text, 'html.parser')
-    #titel_2 = after_login_2.title
-    #print(titel)
-    #print(titel_2)
+    login(url_login)
+    seite = session.get(url_meine_Kurse)
+    seite.text
+    soup = BeautifulSoup(seite.text, 'html.parser')
 
-    ##laden der Startseite in Ojekt
-    #seite = session.get(url_meine_Kurse)
-    #seite.text
-    #soup = BeautifulSoup(seite.text, 'html.parser')
+    hyperlinks = []
 
+    for links in soup.find_all('a'):
+        #print(soup.find_all('a'),list)
+        hyperlinks.append(links.get('href'))
 
-    #soup.find_all('html_elemen', class_='qa-blocklist unlist')
+    #kürzen der Liste der Links
+    kursliste = hyperlinks[hyperlinks.index('#sb-9')+1:hyperlinks.index('#sb-4')+1]
 
 
     print('fertig')
+
+
